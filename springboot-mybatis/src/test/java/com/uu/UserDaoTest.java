2f7656398a4c6e1115b0ef45d86fb8d9b86ccff7
liupenghao <liupenghao1205@163.com>
2018-03-19 16:23:36 +0800

    整合thymeleaf

---
+++ springboot-mybatis/src/test/java/com/uu/UserDaoTest.java
@@ -0,0 +1,34 @@
+package com.uu;
+
+import com.uu.dao.UserDao;
+import com.uu.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 描述：
+ *
+ * @author uusao
+ * @create 2018-03-19 11:12
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserDaoTest {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testInsert(){
+        User user = new User();
+        user.setId(13l);
+        user.setAge(12);
+        user.setNickName("小红");
+        user.setEmail("@16.com");
+        user.setRegTime("dd");
+        userDao.insertSelective(user);
+    }
+}
---
+++ springboot-thymeleaf/Readme.md
@@ -0,0 +1,1 @@
+### SpringBoot 整合 thymeleaf 页面框架
