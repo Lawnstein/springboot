bfd6812ec62404fdbc3349ff18cc69c40f618cac
liupenghao <anhusky@163.com>
2018-10-11 23:31:46 +0800

    springboot 默认定时任务

--- springdata-scheduling/src/main/java/com/uu/husky/task/SchedulerTask.java
+++ springdata-scheduling/src/main/java/com/uu/husky/task/SchedulerTask.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 public class SchedulerTask {
     private int count = 0;
 
-    @Scheduled(cron="*/6 * * * * ?")
+    @Scheduled(cron="0/6 * * * * ?")
     private void process(){
         System.out.println("this is scheduler task runing  "+(count++));
     }
